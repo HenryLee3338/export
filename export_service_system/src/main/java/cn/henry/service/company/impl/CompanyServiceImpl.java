fcb4a3d28e26c9a8aed5610e90f73ee5dee99315
HenryLee3338 <59187351+HenryLee3338@users.noreply.github.com>
2020-01-20 15:10:47 +0800

    Update CompanyServiceImpl.java

--- export_service_system/src/main/java/cn/henry/service/company/impl/CompanyServiceImpl.java
+++ export_service_system/src/main/java/cn/henry/service/company/impl/CompanyServiceImpl.java
@@ -23,7 +23,8 @@ public class CompanyServiceImpl implements CompanyService {
     }
     //分页查询
     public PageInfo findByPageHelper(int page, int size) {
-        System.out.println("测试");
+        System.out.println("测试推送");
+        System.out.println("测试拉取");
         //1、调用PageHelper.startPage方法
         PageHelper.startPage(page, size);
         //2、紧跟着第一个查询会被分页
